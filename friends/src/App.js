7f31698912a3aa0d9b0fadec855c5b564a13af50
Zooheck <davidscottflack@gmail.com>
2019-01-23 15:05:00 -0600

    Add axios call and map over new state

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios'
+import Friend from './components/Friend';
 class App extends Component {
+  state = {
+    friends: []
+  }
+  componentDidMount = () => {
+    axios
+      .get('http://localhost:5000/friends')
+      .then(response => {
+        console.log(response.data)
+        this.setState({
+          friends: response.data
+        })
+      })
+
+  }
   render() {
     return (
       <div className="App">
-
+        <h1>Friend list:</h1>
+        {this.state.friends.map(friend => {
+          return <Friend />
+        })}
       </div>
     );
   }
---
+++ friends/src/components/Friend.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Friend = (props) => {
+    return(
+        <div>ayyyyy</div>
+    )
+}
+
+export default Friend
